418a0f22ff5523c58bd43b747d8daa12929ab976
GalaxyStrafes <gstrafesgaming@gmail.com>
2020-10-20 16:13:03 +0200

    I CODE SHEET

--- app.js
+++ app.js
@@ -1,1 +1,83 @@
-//hello
+var turn = 'O'
+function putshit1() {
+    document.getElementById('1').innerText = turn
+    if (turn == 'O'){
+        turn = 'X'
+    } else if(turn == 'X'){
+        turn = 'O'
+    }
+}
+
+function putshit11() {
+    document.getElementById('1').innerText = turn
+    if (turn == 'O'){
+        turn = 'X'
+    } else if(turn == 'X'){
+        turn = 'O'
+    }
+}
+
+function putshit111() {
+    document.getElementById('1').innerText = turn
+    if (turn == 'O'){
+        turn = 'X'
+    } else if(turn == 'X'){
+        turn = 'O'
+    }
+}
+
+function putshit2() {
+    document.getElementById('1').innerText = turn
+    if (turn == 'O'){
+        turn = 'X'
+    } else if(turn == 'X'){
+        turn = 'O'
+    }
+}
+
+function putshit22() {
+    document.getElementById('1').innerText = turn
+    if (turn == 'O'){
+        turn = 'X'
+    } else if(turn == 'X'){
+        turn = 'O'
+    }
+}
+
+function putshit222() {
+    document.getElementById('1').innerText = turn
+    if (turn == 'O'){
+        turn = 'X'
+    } else if(turn == 'X'){
+        turn = 'O'
+    }
+}
+
+function putshit3() {
+    document.getElementById('1').innerText = turn
+    if (turn == 'O'){
+        turn = 'X'
+    } else if(turn == 'X'){
+        turn = 'O'
+    }
+}
+
+function putshit33() {
+    document.getElementById('1').innerText = turn
+    if (turn == 'O'){
+        turn = 'X'
+    } else if(turn == 'X'){
+        turn = 'O'
+    }
+}
+
+function putshit333() {
+    document.getElementById('1').innerText = turn
+    if (turn == 'O'){
+        turn = 'X'
+    } else if(turn == 'X'){
+        turn = 'O'
+    }
+}
+
+

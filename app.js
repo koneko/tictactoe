abbe45600a008b49e749818c80f73354019799b7
GalaxyStrafes <gstrafesgaming@gmail.com>
2020-10-20 16:35:20 +0200

    javascript is hard, help me dude

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 var turn = 'O'
+var start = 'true'
+
+document.getElementsByClassName('buton').innerText = ""
 function putshit1() {
-    document.getElementById('1').innerText = turn
+    document.getElementById('1').innerText = turn;
+    document.getElementById('1').onclick = "";
+    
     if (turn == 'O'){
         turn = 'X'
     } else if(turn == 'X'){
@@ -9,7 +14,8 @@ function putshit1() {
 }
 
 function putshit11() {
-    document.getElementById('1').innerText = turn
+    document.getElementById('11').innerText = turn;
+    document.getElementById('11').onclick = "";
     if (turn == 'O'){
         turn = 'X'
     } else if(turn == 'X'){
@@ -18,7 +24,8 @@ function putshit11() {
 }
 
 function putshit111() {
-    document.getElementById('1').innerText = turn
+    document.getElementById('111').innerText = turn;
+    document.getElementById('111').onclick = "";
     if (turn == 'O'){
         turn = 'X'
     } else if(turn == 'X'){
@@ -27,7 +34,8 @@ function putshit111() {
 }
 
 function putshit2() {
-    document.getElementById('1').innerText = turn
+    document.getElementById('2').innerText = turn;
+    document.getElementById('2').onclick = "";
     if (turn == 'O'){
         turn = 'X'
     } else if(turn == 'X'){
@@ -36,7 +44,8 @@ function putshit2() {
 }
 
 function putshit22() {
-    document.getElementById('1').innerText = turn
+    document.getElementById('22').innerText = turn;
+    document.getElementById('22').onclick = "";
     if (turn == 'O'){
         turn = 'X'
     } else if(turn == 'X'){
@@ -45,7 +54,8 @@ function putshit22() {
 }
 
 function putshit222() {
-    document.getElementById('1').innerText = turn
+    document.getElementById('222').innerText = turn;
+    document.getElementById('222').onclick = "";
     if (turn == 'O'){
         turn = 'X'
     } else if(turn == 'X'){
@@ -54,7 +64,8 @@ function putshit222() {
 }
 
 function putshit3() {
-    document.getElementById('1').innerText = turn
+    document.getElementById('3').innerText = turn;
+    document.getElementById('3').onclick = "";
     if (turn == 'O'){
         turn = 'X'
     } else if(turn == 'X'){
@@ -63,7 +74,8 @@ function putshit3() {
 }
 
 function putshit33() {
-    document.getElementById('1').innerText = turn
+    document.getElementById('33').innerText = turn;
+    document.getElementById('33').onclick = "";
     if (turn == 'O'){
         turn = 'X'
     } else if(turn == 'X'){
@@ -72,7 +84,8 @@ function putshit33() {
 }
 
 function putshit333() {
-    document.getElementById('1').innerText = turn
+    document.getElementById('333').innerText = turn;
+    document.getElementById('333').onclick = "";
     if (turn == 'O'){
         turn = 'X'
     } else if(turn == 'X'){
@@ -81,3 +94,16 @@ function putshit333() {
 }
 
 
+// rows
+// top row
+// middle row
+// bottom row
+// collums
+// left
+// middle
+// right
+// diagonals
+// left
+// right
+
+// when a player wins, make them not clickable and display win message (and refresh page)
